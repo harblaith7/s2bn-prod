9b521b2e6bad05e6fdd9598029a6af2dfe088a9e
Laith Harb <harblaith77@gmail.com>
2020-08-23 13:31:27 -0400

    fixed keys

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
-    MongoURI = process.env.MONGO_URI,
-    JWTSecret = process.env.JWT_SECRET,
-    authCode = process.env.AUTH_CODE,
-    stripeAPIKey = process.env.STRIPE_API_KEY,
-    stripeSecret = process.env.STRIPE_SECRET
+    MongoURI : process.env.MONGO_URI,
+    JWTSecret : process.env.JWT_SECRET,
+    authCode : process.env.AUTH_CODE,
+    stripeAPIKey : process.env.STRIPE_API_KEY,
+    stripeSecret : process.env.STRIPE_SECRET
 }
